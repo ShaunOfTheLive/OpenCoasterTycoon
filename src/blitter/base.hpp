f3efb4dcd793e6dca9a6c5aa0d1b31f8110ffb8d
ShaunOfTheLive <ShaunOfTheLive@08261c64-e8fe-4da2-b1bf-e1e8d53b29a5>
2012-02-07 03:29:01 +0000

    undo r3

--- src/blitter/base.hpp
+++ src/blitter/base.hpp
@@ -1,10 +1,10 @@
 /* $Id: base.hpp 21782 2011-01-14 15:34:33Z frosch $ */
 
 /*
- * This file is part of OpenCoaster Tycoon.
+ * This file is part of OpenTTD.
  * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
  * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenCoaster Tycoon. If not, see <http://www.gnu.org/licenses/>.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /** @file base.hpp Base for all blitters. */
